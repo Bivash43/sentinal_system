4da8dd35ec393874aa22c32cc21a57d046fdf841
Bivash <bivash43e@gmail.com>
2026-04-09 20:52:42 +0100

    :fire: style: remove unused import

--- sentinal_backend/src/test/java/com/example/sentinal_backend/SentinalBackendApplicationTests.java
+++ sentinal_backend/src/test/java/com/example/sentinal_backend/SentinalBackendApplicationTests.java
@@ -1,7 +1,7 @@
 package com.example.sentinal_backend;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+
 
 class SentinalBackendApplicationTests extends AbstractIntegrationTest {
 
